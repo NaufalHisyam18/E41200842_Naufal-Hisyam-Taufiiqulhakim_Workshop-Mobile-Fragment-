c9955417c38696e8d2c7c07224ee38a2182de3f3
NaufalHisyam18 <naufalhisyam84@gmail.com>
2022-03-17 15:24:15 +0700

    Create .gitignore

---
+++ beautyhouse/src/Login.jsx
@@ -0,0 +1,16 @@
+import  "bootstrap/dist/css/bootstrap.css";
+//import React from "react";
+import "./Login.css";
+import React, {Component} from 'react';
+ 
+class Login  extends Component {
+    render() {
+        return (
+            <div className="main-body">
+                <div className="body">
+                </div>
+            </div> 
+         );
+    }
+}
+export default Login;
